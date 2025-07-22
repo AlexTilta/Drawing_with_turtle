b943b40b780d35c7442feb0885dfe6cf7c8be8ee
AlexTilta <aksyonow.alexander2016@yandex.ru>
2025-07-22 20:30:23 +0500

    add romb_func

--- Basic_figures.py
+++ Basic_figures.py
@@ -2,9 +2,18 @@ from turtle import *
 
 t = Turtle()
 
-def square(n):
+def square(side):
     for i in range(4):
-        t.forward(n)
+        t.forward(side)
         t.left(90)
-square(50)
+
+def romb(side, angle):
+    """Функция реализует ромб с одним заданным углом и стороной"""
+    for i in range(2):
+        t.forward(side)
+        t.left(angle)
+        t.forward(side)
+        t.left(180 - angle)
+
+romb(50, 60)
 t.screen.mainloop()
